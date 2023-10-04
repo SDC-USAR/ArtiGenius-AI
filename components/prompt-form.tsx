e6ca2d61c1f02b5fca95ad58362fafabd5990b26
Ademílson F. Tonato <ademilsonft@outlook.com>
2023-10-04 09:07:40 +0000

    feat: adding darktheme support for input

--- components/prompt-form.tsx
+++ components/prompt-form.tsx
@@ -1,122 +1,136 @@
-import React from "react";
-import * as Form from "@radix-ui/react-form";
-import { styled } from "@stitches/react";
+import React from 'react';
+import { useTheme } from 'next-themes';
+import * as Form from '@radix-ui/react-form';
+import { styled } from '@stitches/react';
 
 type Props = {
-    onSubmit: (prompt: string) => void;
-    isGenerating: boolean;
+  onSubmit: (prompt: string) => void;
+  isGenerating: boolean;
 };
+
+type ThemeVariants = 'dark' | 'light';
 
 export const PromptForm: React.FC<Props> = ({ onSubmit, isGenerating }) => {
-    const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
-        e.preventDefault();
+  const { theme, resolvedTheme } = useTheme();
+  const currentTheme: ThemeVariants = (theme || resolvedTheme || 'light') as ThemeVariants;
 
-        const prompt = (e.target as HTMLFormElement | undefined)?.prompt
-            ?.value as string;
-        onSubmit(prompt);
-    };
-    return (
-        <FormRoot onSubmit={handleSubmit}>
-            <FormField name="email">
-                <Flex
-                    css={{
-                        alignItems: "baseline",
-                        justifyContent: "space-between",
-                    }}
-                >
-                    <FormLabel>Prompt</FormLabel>
-                    <FormMessage match="valueMissing">
-                        Please enter a prompt
-                    </FormMessage>
-                    <FormMessage match="typeMismatch">
-                        Please enter a valid prompt
-                    </FormMessage>
-                </Flex>
-                <Form.Control asChild>
-                    <Input
-                        type="text"
-                        name="prompt"
-                        required
-                        placeholder="Enter a prompt like 'a painting of a cat'"
-                    />
-                </Form.Control>
-            </FormField>
-            <Form.Submit asChild>
-                <Button css={{ marginTop: 10 }} disabled={isGenerating}>
-                    {isGenerating ? "Generating.." : "Start Generating"}
-                </Button>
-            </Form.Submit>
-        </FormRoot>
-    );
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+
+    const prompt = (e.target as HTMLFormElement | undefined)?.prompt?.value as string;
+    onSubmit(prompt);
+  };
+
+  return (
+    <StyledFormRoot onSubmit={handleSubmit}>
+      <StyledFormField name="email">
+        <StyledFlex css={{ alignItems: 'baseline', justifyContent: 'space-between' }}>
+          <StyledFormLabel>Prompt</StyledFormLabel>
+          <StyledFormMessage match="valueMissing">Please enter a prompt</StyledFormMessage>
+          <StyledFormMessage match="typeMismatch">Please enter a valid prompt</StyledFormMessage>
+        </StyledFlex>
+        <Form.Control asChild>
+          <StyledInput
+            type="text"
+            name="prompt"
+            required
+            placeholder="Enter a prompt like 'a painting of a cat'"
+            autoComplete="off"
+            theme={currentTheme}
+          />
+        </Form.Control>
+      </StyledFormField>
+      <Form.Submit asChild>
+        <StyledButton css={{ marginTop: 10 }} disabled={isGenerating}>
+          {isGenerating ? 'Generating..' : 'Start Generating'}
+        </StyledButton>
+      </Form.Submit>
+    </StyledFormRoot>
+  );
 };
 
-const FormRoot = styled(Form.Root, {});
+const StyledFormRoot = styled(Form.Root, {});
 
-const FormField = styled(Form.Field, {
-    display: "grid",
-    marginBottom: 10,
+const StyledFormField = styled(Form.Field, {
+  display: 'grid',
+  marginBottom: 10,
 });
 
-const FormLabel = styled(Form.Label, {
-    fontSize: 15,
-    fontWeight: 500,
-    lineHeight: "35px",
-    color: "$foreground",
+const StyledFormLabel = styled(Form.Label, {
+  fontSize: 15,
+  fontWeight: 500,
+  lineHeight: '35px',
+  color: '$foreground',
 });
 
-const FormMessage = styled(Form.Message, {
-    fontSize: 13,
-    color: "$red600",
-    opacity: 0.8,
+const StyledFormMessage = styled(Form.Message, {
+  fontSize: 13,
+  color: '$red600',
+  opacity: 0.8,
 });
 
-const Flex = styled("div", { display: "flex" });
+const StyledFlex = styled('div', { display: 'flex' });
 
 const inputStyles = {
-    all: "unset",
-    boxSizing: "border-box",
-    width: "100%",
-    display: "inline-flex",
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 4,
+  all: 'unset',
+  boxSizing: 'border-box',
+  width: '100%',
+  display: 'inline-flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  borderRadius: 4,
 
-    fontSize: 15,
-    color: "black",
-    backgroundColor: "$gray300",
-    boxShadow: `0 0 0 1px $gray400`,
-    "&:hover": { boxShadow: `0 0 0 1px $gray600` },
-    "&:focus": { boxShadow: `0 0 0 2px $purple600` },
-    "&::selection": { backgroundColor: "$gray600", color: "white" },
+  fontSize: 15,
+  color: 'black',
+  backgroundColor: '$gray300',
+  boxShadow: '0 0 0 1px $gray400',
+  '&:hover': { boxShadow: '0 0 0 1px $gray600' },
+  '&:focus': { boxShadow: '0 0 0 2px $purple600' },
+  '&::selection': { backgroundColor: '$gray600', color: 'white' },
 };
 
-const Input = styled("input", {
-    ...inputStyles,
-    height: 35,
-    lineHeight: 1,
-    padding: "0 10px",
+const StyledInput = styled('input', {
+  ...inputStyles,
+  height: 35,
+  lineHeight: 1,
+  padding: '0 10px',
+  variants: {
+    theme: {
+      dark: {
+        backgroundColor: '$gray600',
+        color: 'white',
+        '&::placeholder': {
+          color: 'white',
+        },
+      },
+      light: {
+        backgroundColor: '$gray300',
+      },
+    },
+  },
 });
 
-const Button = styled("button", {
-    all: "unset",
-    boxSizing: "border-box",
-    display: "inline-flex",
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 4,
-    padding: "0 15px",
-    fontSize: 15,
-    lineHeight: 1,
-    fontWeight: 500,
-    height: 35,
-    width: "100%",
+const StyledButton = styled('button', {
+  all: 'unset',
+  boxSizing: 'border-box',
+  display: 'inline-flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  borderRadius: 4,
+  padding: '0 15px',
+  fontSize: 15,
+  lineHeight: 1,
+  fontWeight: 500,
+  height: 35,
+  width: '100%',
+  cursor: 'pointer',
 
-    "&:disabled": {
-        opacity: 0.5,
-    },
-    backgroundColor: "$purple500",
-    color: "white",
-    boxShadow: `0 2px 10px $gray400`,
-    "&:not(:disabled):hover": { backgroundColor: "$purple600" },
-    "&:not(:disabled):focus": { boxShadow: `0 0 0 2px black` },
+  '&:disabled': {
+    opacity: 0.5,
+  },
+  backgroundColor: '$purple500',
+  color: 'white',
+  boxShadow: '0 2px 10px $gray400',
+  '&:not(:disabled):hover': { backgroundColor: '$purple600' },
+  '&:not(:disabled):focus': { boxShadow: '0 0 0 2px black' },
 });
